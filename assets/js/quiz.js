10c72a4f7068aa46129eef3eb8bccdd466802099
Carlos Colombo <csc7@hw.ac.uk>
2021-07-22 03:06:09 +0000

    Add first functions and question to quiz.js file

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,19 +6,33 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let startButton = document.getElementById("start-button");
-    startButton.addEventListener("click", timeSpent);
+    startButton.addEventListener("click", startQuiz);
     let sendButton = document.getElementById("send-button");
     sendButton.addEventListener("click", sendAnswer);
+    
+
+    document.getElementById("answer").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            sendAnswer();
+        }
+    });
 });
 
 
-
-
-
-function timeSpent() {
+function startQuiz() {
     console.log("Start button clicked");
+    document.getElementById("answer").value = "";
+    document.getElementById("answer").focus();
+    let x1 = Math.floor(Math.random() * 100) + 1;
+    let x2 = x1 * 5;
+    displayQuestion(x1, x2);
 }
 
 function sendAnswer() {
     console.log("Send button clicked");
+}
+
+
+function displayQuestion (num, div) {
+    document.getElementById('question').textContent = "La division ${num} dividido ${div} es?";
 }
