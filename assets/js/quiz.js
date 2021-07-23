de858193d8797cf32be7742c2e8e9ae8c4c420c4
Carlos Colombo <csc7@hw.ac.uk>
2021-07-23 02:10:13 +0000

    Add timer and complete reset functions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,10 +5,14 @@
 // and function to add event listeners to button elements
 
 var i = 0;
+var totalCorrect = 0;
+var startTime = 0;
+var t = 0;
+
 
 document.addEventListener("DOMContentLoaded", function() {
     let startButton = document.getElementById("start-button");
-        startButton.addEventListener("click", runQuiz);
+        startButton.addEventListener("click", initialiseQuiz);
     let sendButton = document.getElementById("send-button");
         sendButton.addEventListener("click", sendAnswer);    
 
@@ -19,37 +23,63 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function initialiseQuiz() {
+    if (i == 0) {
+        runQuiz();
+    } else {
+        resetQuiz();
+        runQuiz();
+    }
+}
+
 function runQuiz() {
     console.log("Start button clicked");
     //console.log("i = " + i);
     document.getElementById("answer").value = "";
     document.getElementById("answer").focus();
+    // Compute time lapse
+    if (i == 0) {
+        startTime = new Date().getTime();
+        t = setInterval (timer, 1000);
+    }
+
+
+    //var startTime = new Date().getTime();;
+    //var currentTime = 
+    //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //document.getElementById('clock').textContent = startTime;
+    //document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+
+    console.log(startTime);
     let firstValue = Math.floor(Math.random() * 100) + 1;
     let secondValue = firstValue * 5;
+    updateProgressBar();
     displayQuestion(firstValue, secondValue);   
 }
 
-function sendAnswer() {
+function sendAnswer() {    
+    i++;
     console.log("Send button clicked");
     let answer = parseInt(document.getElementById("answer").value);
     console.log(answer);
     let correctAnswer = getCorrectAnswer();
     if (answer === correctAnswer) {
         alert("Hey! You got it right! :D");
-        addPoint();
-        updateProgressBar();
+        totalCorrect++;
+        document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} out of ${i}`;
     } else {
         alert(`Awwww.... you answered ${answer}. The correct answer was ${correctAnswer}!`);
+        document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} out of ${i}`;
     }
-    i++;
+    
     if (i == 10) {
-        alert(`Your score is ${answer} achieved in ${answer} minutes`);
-        i = 0;
-        document.getElementById("answer").value = null;
-        document.getElementById("answer").focus();
-    } else {
-        runQuiz();
+        resetQuiz();
+    } else {        
+        runQuiz();        
     }
+    
 }
 
 function displayQuestion (num, division) {
@@ -107,5 +137,27 @@ function nextQuestion () {
 }
 
 function updateProgressBar() {
+    let fillProgress = document.getElementById('prog-bar').children[i];
+    fillProgress.style.backgroundColor = "blue";
+}
 
+function resetQuiz() {
+    alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${i} minutes`);
+    i = 0;
+    document.getElementById("answer").value = null;
+    document.getElementById("answer").focus();
+    for (j = 0; j < 10; j++) {
+        document.getElementById('prog-bar').children[j].style.backgroundColor = "rgb(148, 157, 240)";
+    }
+    document.getElementById('prog-bar').children[0].style.backgroundColor= "blue";
+    clearInterval(t);
+    startTime = new Date().getTime();
+    t = setInterval (timer, 1000);
+}
+
+function timer() {
+    let currentTime = new Date().getTime();
+
+    let seconds = Math.floor(((currentTime - startTime)/1000));
+    document.getElementById("clock").innerHTML = seconds;
 }
