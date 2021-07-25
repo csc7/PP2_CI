51eaa85b8528be809e97329880b23e7d0fb2b96b
Carlos Colombo <csc7@hw.ac.uk>
2021-07-25 03:30:04 +0000

    Test export button

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -2,11 +2,16 @@
 // Event listener to check when the DOM has been loaded completely
 // and function to add event listeners to button elements
 
-const exp = require("constants");
+//const exp = require("constants");
 
 document.addEventListener("DOMContentLoaded", function() {
     let generateButton = document.getElementById("generate-button");
     generateButton.addEventListener("click", generateGraph);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    let generateButton = document.getElementById("export-button");
+    generateButton.addEventListener("click", exportFile);
 });
 
 
@@ -15,10 +20,18 @@ document.getElementById("sampling-field").value = "2";
 document.getElementById("length-field").value = "100";
 document.getElementById("frequency-field").value = "50";
 
+
 function computeGraphData() {
-    let samplingRate = parseInt(document.getElementById('sampling-field').innerText);
-    let waveletLength = parseInt(document.getElementById('length-field').innerText);
-    let frequency = parseInt(document.getElementById('frequency-field').innerText);
+
+    let samplingRate = parseInt(document.getElementById('sampling-field').value);
+    let waveletLength = parseInt(document.getElementById('length-field').value);
+    let frequency = parseInt(document.getElementById('frequency-field').value);
+    totalSamples = Math.floor(waveletLength / samplingRate) + 1;
+
+    let timeVector = []; 
+    let amplitude = [];
+    let dataForGraph = [];
+    dataForGraph.push(["Time", "Amplitude"]);
     
     if (samplingRate == 0 || waveletLength == 0) {
         alert(`Sammpling rate or wavelet length cannot be zero. Please assign a different value.`);
@@ -27,23 +40,20 @@ function computeGraphData() {
                Consider the former much longer that the latter for better results.
                Please assign different values`);
     } else {
-        let timeVector = 0; // Initialization of timeVector with zero at the first sample (zero time)
+        // = 0; // Initialization of timeVector with zero at the first sample (zero time)
         // Time Vector including previous first sample and the rest
-        for (let i = 1; i < (Match.floor(waveletLength / samplingRate) + 1); i++) {
-            let timeVector = timeVector.push(samplingRate * i);            
+        for (let i = 0; i < totalSamples; i++) {
+            timeVector.push(samplingRate * i);            
         }
         // Computation of amplitudes for each time
-        let amplitude = 0;// Initialization of amplitude with corresponding value at the first sample (zero time)
-        for (let j = 0; j < (Match.floor(aveletLength / samplingRate) + 1); j++) {
-            let amplitude = (1 - 0.5 * frequency * frequency * timeVector[i] * timeVector[i]) * exp((-1) * frequency * frequency * timeVector[i] * timeVector[i]);
-            amplitude.push(amplitude);            
-        }
+         // = 0;// Initialization of amplitude with corresponding value at the first sample (zero time)
+        for (let j = 0; j < totalSamples; j++) {
+            amplitude.push(1 - 0.5 * frequency * frequency * timeVector[j] * timeVector[j]) * Math.exp((-1) * frequency * frequency * timeVector[j] * timeVector[j]);
+            dataForGraph.push([timeVector[j], amplitude[j]]);     
+        }        
     }
-
+    return dataForGraph;
 }
-
-
-
 
 
 // GRAPH
@@ -53,24 +63,13 @@ function computeGraphData() {
 
 function generateGraph() {
 
+    dataForGoogleChartFunction = computeGraphData();
+
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ['Year', 'Sales'],
-        ['2004',  1000, ],
-        ['2005',  1170, ],
-        ['2006',  660,  ],
-        ['2007',  1030, ],
-        ['2008',  660,  ],
-        ['2009',  660,  ],
-        ['2010',  660,  ],
-        ['2011',  660,  ],
-        ['2012',  660,  ],
-        ['2013',  660,  ],
-
-      ]);
+      var data = google.visualization.arrayToDataTable(dataForGoogleChartFunction);
 
       var options = {
         //title: 'Company Performance',
@@ -82,5 +81,21 @@ function generateGraph() {
 
       chart.draw(data, options);
     }
+
+}
+
+
+
+
+
+
+function exportFile() {
+    // Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+    console.log("OK")
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
 
 }
