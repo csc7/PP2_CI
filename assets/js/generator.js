64cf4032358499be6d8fa2e6218b6965c917dd31
Carlos Colombo <csc7@hw.ac.uk>
2021-07-25 00:58:50 +0000

    Add frequency field in control panel and write Ricker function to cumpute data

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -1,7 +1,49 @@
+// COPIED FROM Code Institute's Love Maths - Essentials Project
+// Event listener to check when the DOM has been loaded completely
+// and function to add event listeners to button elements
+
+const exp = require("constants");
+
 document.addEventListener("DOMContentLoaded", function() {
-    let startButton = document.getElementById("start-button");
-    startButton.addEventListener("click", initialiseQuiz);
+    let generateButton = document.getElementById("generate-button");
+    generateButton.addEventListener("click", generateGraph);
 });
+
+
+document.getElementById("wavelet-type-field").value = "Haar";
+document.getElementById("sampling-field").value = "2";
+document.getElementById("length-field").value = "100";
+document.getElementById("frequency-field").value = "50";
+
+function computeGraphData() {
+    let samplingRate = parseInt(document.getElementById('sampling-field').innerText);
+    let waveletLength = parseInt(document.getElementById('length-field').innerText);
+    let frequency = parseInt(document.getElementById('frequency-field').innerText);
+    
+    if (samplingRate == 0 || waveletLength == 0) {
+        alert(`Sammpling rate or wavelet length cannot be zero. Please assign a different value.`);
+    } else if (samplingRate >= waveletLength) {
+        alert(`Wavelet length should be greater than sampling rate.
+               Consider the former much longer that the latter for better results.
+               Please assign different values`);
+    } else {
+        let timeVector = 0; // Initialization of timeVector with zero at the first sample (zero time)
+        // Time Vector including previous first sample and the rest
+        for (let i = 1; i < (Match.floor(waveletLength / samplingRate) + 1); i++) {
+            let timeVector = timeVector.push(samplingRate * i);            
+        }
+        // Computation of amplitudes for each time
+        let amplitude = 0;// Initialization of amplitude with corresponding value at the first sample (zero time)
+        for (let j = 0; j < (Match.floor(aveletLength / samplingRate) + 1); j++) {
+            let amplitude = (1 - 0.5 * frequency * frequency * timeVector[i] * timeVector[i]) * exp((-1) * frequency * frequency * timeVector[i] * timeVector[i]);
+            amplitude.push(amplitude);            
+        }
+    }
+
+}
+
+
+
 
 
 // GRAPH
@@ -9,26 +51,36 @@ document.addEventListener("DOMContentLoaded", function() {
 // Line Charts
 // Copied and modified from https://developers.google.com/chart/interactive/docs/gallery/linechart on July 24th, 2021, at 23:06.
 
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+function generateGraph() {
 
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Year', 'Sales', 'Expenses'],
-    ['2004',  1000,      400],
-    ['2005',  1170,      460],
-    ['2006',  660,       1120],
-    ['2007',  1030,      540]
-  ]);
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
 
-  var options = {
-    title: 'Company Performance',
-    curveType: 'function',
-    legend: { position: 'bottom' }
-  };
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales'],
+        ['2004',  1000, ],
+        ['2005',  1170, ],
+        ['2006',  660,  ],
+        ['2007',  1030, ],
+        ['2008',  660,  ],
+        ['2009',  660,  ],
+        ['2010',  660,  ],
+        ['2011',  660,  ],
+        ['2012',  660,  ],
+        ['2013',  660,  ],
 
-  var chart = new google.visualization.LineChart(document.getElementById('curve_chart-top'));
+      ]);
 
-  chart.draw(data, options);
+      var options = {
+        //title: 'Company Performance',
+        //curveType: 'function',
+        legend: { position: 'bottom' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('curve_chart-top'));
+
+      chart.draw(data, options);
+    }
+
 }
-
