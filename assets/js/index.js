3c264fb296909fc80d85d45cf6f9036ec309464e
Carlos Colombo <csc7@hw.ac.uk>
2021-07-31 04:00:48 +0000

    Create index.js file and add code to move content when clicking Bootstrap hamburguer button

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+// Move content down when clicking the Bootstrap hamburguer
+// button in the fixed navigation menu
+let hamburguerButton = document.getElementsByTagName("button")[0];
+hamburguerButton.addEventListener("click", moveContent);
+
+//#header > nav > button
+//document.getElementsByTagName("button")[0]
+
+function moveContent () {
+    if (document.getElementsByClassName("navbar-toggler")[0].getAttribute("aria-expanded") == "false") {
+        let firstElementToMove = document.getElementById('functions-page-heading');
+        firstElementToMove.style.marginTop = "200px";
+        let secondElementToMove = document.getElementById('home-body');
+        secondElementToMove.style.marginTop = "200px";
+    } else {
+        let firstElementToMove = document.getElementById('functions-page-heading');
+        firstElementToMove.style.marginTop = "0";
+        let secondElementToMove = document.getElementById('home-body');
+        secondElementToMove.style.marginTop = "0";
+    }
+}
