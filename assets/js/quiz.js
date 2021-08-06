97701997a511b2a214bbf21758614f7e5b85086d
Carlos Colombo <csc7@hw.ac.uk>
2021-08-06 00:51:53 +0000

    Bring quiz.js back to original state after confirming scripts are loaded in IE browser

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -69,7 +69,7 @@ function runQuiz() {
     if (i == 0) {
         startTime = new Date().getTime();
         t = setInterval (timer, 1000);
-        //document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} / ${i}`;
+        document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} / ${i}`;
         for (let j = 0; j < 10; j++) {
             document.getElementById('prog-bar').children[j].style.backgroundColor = "rgb(148, 157, 240)";
         }
@@ -92,10 +92,10 @@ function sendAnswer() {
     if (answer === correctAnswer) {
         totalCorrect++;
         document.getElementById('last-answer').children[1].textContent = answer;
-        //document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} / ${i}`;
+        document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} / ${i}`;
     } else {
         document.getElementById('last-answer').children[1].textContent = answer;
-        //document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} / ${i}`;
+        document.getElementById('score-div').children[1].textContent = ` ${totalCorrect} / ${i}`;
     }
     document.getElementById('last-correct-answer').children[1].textContent = correctAnswer;    
     // Check if last question is reached; if it is, send alert, give summary and disable send button until it is restarted
@@ -103,16 +103,16 @@ function sendAnswer() {
         clearInterval(t);        
         document.getElementById('prog-bar').children[i-1].style.backgroundColor = "blue";        
         let timeSpent = document.getElementById("clock").textContent;
-        //// Same message, there are many options to choose from to avoid showing incorrect format of minutes and seconds, e.g., avoid showing seconds with three digits
-        //if (timeSpent[0] == 0 && timeSpent[5] == 0) {
-        //    alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[1]} minutes and ${timeSpent[6]} seconds).`);
-        //} else if (timeSpent[0] == 0 && timeSpent[5] != 0) {
-        //    alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[1]} minutes and ${timeSpent[5]}${timeSpent[6]} seconds).`);
-        //} else if (timeSpent[0] != 0 && timeSpent[5] == 0) {
-        //    alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[0]}${timeSpent[1]} minutes and ${timeSpent[6]} seconds).`);
-        //} else {
-        //    alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[0]}${timeSpent[1]} minutes and ${timeSpent[5]}${timeSpent[6]} seconds).`);
-        //}      
+        // Same message, there are many options to choose from to avoid showing incorrect format of minutes and seconds, e.g., avoid showing seconds with three digits
+        if (timeSpent[0] == 0 && timeSpent[5] == 0) {
+            alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[1]} minutes and ${timeSpent[6]} seconds).`);
+        } else if (timeSpent[0] == 0 && timeSpent[5] != 0) {
+            alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[1]} minutes and ${timeSpent[5]}${timeSpent[6]} seconds).`);
+        } else if (timeSpent[0] != 0 && timeSpent[5] == 0) {
+            alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[0]}${timeSpent[1]} minutes and ${timeSpent[6]} seconds).`);
+        } else {
+            alert(`You answered ${i} questions, with ${totalCorrect} correct. You took ${timeSpent[0]}${timeSpent[1]}:${timeSpent[5]}${timeSpent[6]} (${timeSpent[0]}${timeSpent[1]} minutes and ${timeSpent[5]}${timeSpent[6]} seconds).`);
+        }      
         document.getElementById("quiz-send-button").disabled = true;    
         document.getElementById("answer").blur();   
         return;                
@@ -150,7 +150,7 @@ function displayQuestion (num) {
                'Hz signal to complete 50 cycles',
                ];
     // Build whole question, inserting a changing value between first and last part of questions, so values are different in each run of quiz
-//    document.getElementById('question').innerHTML = `${questionStart[i]} <span id="first-value">${num}</span>${questionEnd[i]}<span id="second-value">? (Give the whole part, largest integer less than or equal to your result).</span>`;
+    document.getElementById('question').innerHTML = `${questionStart[i]} <span id="first-value">${num}</span>${questionEnd[i]}<span id="second-value">? (Give the whole part, largest integer less than or equal to your result).</span>`;
 }
 
 // Get the correct answer based on the question previously generated
